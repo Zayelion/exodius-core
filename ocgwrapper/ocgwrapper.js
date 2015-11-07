f12dcb98b29fffd36d2ab8138db336f435a49c8f
Jamezs Gladney <jamezs@angelofcode.com>
2015-11-07 04:02:33 -0500

    ocgcore connection runs
    - appears to be a proper connection to the ocgcore.dll
    - the way the callback functions are initiated doesnt seem right

--- ocgwrapper/ocgwrapper.js
+++ ocgwrapper/ocgwrapper.js
@@ -2,28 +2,12 @@
 
 'use strict';
 
-var sqlite3 = require('sqlite3').verbose(),
-    /* access databse file */
-    fs = require('fs'),
-    /* access file system */
-    ffi = require('ffi'),
-    /* allows dynamic linking of the ocgapi.dll, critical; */
-    ref = require('ref'),
-    /* allows use of C++ pointers for C++ JS interactions, critical */
-    struct = require('ref-struct');
-/* allows use of C++ structures for C++ JS interactions, critical 
-queryfor = require('./sql-queries');*/
-var dllLocation = (__dirname + '\\ocgcore-64.dll');
-
-function PointerConstructor(name) {
-    return {
-        size: 1, //default is a byte
-        indirection: 1,
-        get: function () {},
-        set: function () {},
-        name: name
-    };
-}
+var sqlite3 = require('sqlite3').verbose(), // access databse file */
+    fs = require('fs'), // access file system */
+    ffi = require('ffi'), // allows dynamic linking of the ocgapi.dll, critical; */
+    ref = require('ref'), // allows use of C++ pointers for C++ JS interactions, critical */
+    struct = require('ref-struct'); // allows use of C++ structures for C++ JS interactions, critical 
+//queryfor = require('./sql-queries'); * /
 
 function constructDatabase(targetDB, targetFolder) {
     // create instance of card database in memory 2MB, prevents sychronous read request and server lag.
@@ -52,18 +36,17 @@ function constructDatabase(targetDB, targetFolder) {
         //function used by the core to process DB
         var code = request.code;
 
-        //        return struct({
-        //            code: code,
-        //            alias: cards[code].alias || 0,
-        //            setcode: cards[code].setcode || 0,
-        //            type: cards[code].type || 0,
-        //            level: cards[code].level || 1,
-        //            attribute: cards[code].attribute || 0,
-        //            race: cards[code].race || 0,
-        //            attack: cards[code].attack || 0,
-        //            defence: cards[code].defense || 0
-        //        });
-        return new Buffer(0);
+        return struct({
+            code: code,
+            alias: cards[code].alias || 0,
+            setcode: cards[code].setcode || 0,
+            type: cards[code].type || 0,
+            level: cards[code].level || 1,
+            attribute: cards[code].attribute || 0,
+            race: cards[code].race || 0,
+            attack: cards[code].attack || 0,
+            defence: cards[code].defense || 0
+        });
     };
 }
 
@@ -97,52 +80,61 @@ function constructScripts(targetFolder) {
         //function used by the core to process scripts
         var filename = 'c' + id,
             output = new Buffer(scripts[filename]);
-        return output;
+        return id;
     };
 }
 
-var settings = {
-    card_reader: constructDatabase('./card.cdb'), //needs to be configurable
-    script_reader: constructScripts('./scripts/') //needs to be configurable
-};
+function initDuel(dllLocation, settings) {
+    var duelptr,
+        seed = Math.floor((Math.random() * 4294967295)), // random uint32
+        cardReader = ffi.Function('uint32', ['uint32', 'pointer'], settings.card_reader), //pointer function for card reader
+        scriptReader = ffi.Function('uint32', ['uint32', 'pointer'], settings.script_reader), // pointer function for script reader
+        messageHandler = ffi.Function('uint32', ['pointer', 'uint32'], console.log), // pointer function for message handler
+        ocgapi = ffi.Library(dllLocation, {
+            'set_script_reader': ['void', [cardReader]],
+            'set_card_reader': ['void', [scriptReader]],
+            'set_message_handler': ['void', ['pointer']],
+            'create_duel': ['pointer', ['uint32']],
+            'start_duel': ['void', ['pointer', 'int']],
+            'end_duel': ['void', ['pointer']],
+            'set_player_info': ['void', ['pointer', 'int32', 'int32', 'int32', 'int32']],
+            'get_log_message': ['void', ['pointer', 'byte*']],
+            'get_message': ['int32', ['pointer', 'pointer']],
+            'process': ['int32', ['pointer']],
+            'new_card': ['void', ['pointer', 'uint32', 'uint8', 'uint8', 'uint8', 'uint8', 'uint8']],
+            'new_tag_card': ['void', ['pointer', 'uint32', 'uint8', 'uint8']],
+            'query_card': ['int32', ['pointer', 'uint8', 'uint8', 'int32', 'byte*', 'int32']],
+            'query_field_count': ['int32', ['pointer', 'uint8', 'uint8']],
+            'query_field_card': ['int32', ['pointer', 'uint8', 'uint8', 'int32', 'byte*', 'int32']],
+            'query_field_info': ['int32', ['pointer', 'byte*']],
+            'set_responsei': ['void', ['pointer', 'int32']],
+            'set_responseb': ['void', ['pointer', 'byte*']],
+            'preload_script': ['int32', ['pointer', 'char*', 'int32']]
+        }); // connect to the dll and expose the functions in it.
 
+    ocgapi.set_card_reader(new Buffer([0])); // connect the pointer functions
+    ocgapi.set_script_reader(new Buffer([0]));
+    ocgapi.set_message_handler(new Buffer([0]));
+    duelptr = ocgapi.create_duel(seed); //generate a duel.
+    return {
+        pointer: duelptr,
+        duel: ocgapi
+    };
+}
 
-
-var connect = function (dllLocation, settings) {
-    // create new instance of flourohydride/ygopro/ocgcore
-    console.log(settings.card_reader);
-    var ocgapi = ffi.Library(dllLocation, {
-        'set_script_reader': ['void', ['pointer']],
-        'set_card_reader': ['void', ['pointer']],
-        'set_message_handler': ['void', ['pointer']],
-        'create_duel': ['pointer', ['uint32']],
-        'start_duel': ['void', ['pointer', 'int']],
-        'end_duel': ['void', ['pointer']],
-        'set_player_info': ['void', ['pointer', 'int32', 'int32', 'int32', 'int32']],
-        'get_log_message': ['void', ['pointer', 'byte*']],
-        'get_message': ['int32', ['pointer', 'pointer']],
-        'process': ['int32', ['pointer']],
-        'new_card': ['void', ['pointer', 'uint32', 'uint8', 'uint8', 'uint8', 'uint8', 'uint8']],
-        'new_tag_card': ['void', ['pointer', 'uint32', 'uint8', 'uint8']],
-        'query_card': ['int32', ['pointer', 'uint8', 'uint8', 'int32', 'byte*', 'int32']],
-        'query_field_count': ['int32', ['pointer', 'uint8', 'uint8']],
-        'query_field_card': ['int32', ['pointer', 'uint8', 'uint8', 'int32', 'byte*', 'int32']],
-        'query_field_info': ['int32', ['pointer', 'byte*']],
-        'set_responsei': ['void', ['pointer', 'int32']],
-        'set_responseb': ['void', ['pointer', 'byte*']],
-        'preload_script': ['int32', ['pointer', 'char*', 'int32']]
+function ocgwrapper(dllLocation, cardDBLocation, scriptFolder) {
+    var settings = {
+        card_reader: constructDatabase(cardDBLocation), //needs to be configurable
+        script_reader: constructScripts(scriptFolder) //needs to be configurable
+    };
+    fs.exists(dllLocation, function (dllDetected) {
+        if (dllDetected) {
+            return initDuel;
+        } else {
+            var errorMessage = '[OCGWrapper]:Error could not find OCGCore at: ' + dllLocation;
+            throw new Error(errorMessage);
+        }
     });
-    console.log(ocgapi.set_script_reader(new Buffer(), function () {
-        console.log('returned');
-    }));
-    return ocgapi;
-};
-fs.exists(dllLocation, function (dllDetected) {
-    if (dllDetected) {
-        connect(dllLocation, settings);
-    } else {
-        console.log('[]:Error could not find OCGCore at:', dllLocation);
-    }
-});
+}
 
-module.exports.core = connect;
+module.exports = ocgwrapper;
